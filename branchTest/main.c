329a438ce81580fd2d668e6183700f4e67be08ff
Huang Rui <rui8907@qq.com>
2025-12-04 16:45:13 +0800

    Add modified C file.

--- branchTest/main.c
+++ branchTest/main.c
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Creating a new branch is quick."); 
+	printf("Creating a new branch is quick and simple."); 
 	return 0;
 }
